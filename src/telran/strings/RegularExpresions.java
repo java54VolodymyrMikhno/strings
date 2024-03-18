c6bc214066b8de07f979632472266349467c1b61
VolodymyrMikhno <volodymyr3583@gmail.com>
2024-03-18 17:29:47 +0200

    replace of repeated code

--- src/telran/strings/RegularExpresions.java
+++ src/telran/strings/RegularExpresions.java
@@ -1,19 +1,24 @@
 package telran.strings;
 
 public class RegularExpresions {
-public static String javaVariable() {
-	String regex ="[A-Za-z$][\\w$]*|_[\\w$]+";
-	return regex;
-}
-public static String zero_300() {
-	String regex = "[1-9]\\d?|[1-2]\\d\\d|300|0"; 
-	return regex;
-}
-public static String ipOctet() {
-	 return "25[0-5]|2[0-4][0-9]|[01]?[0-9]{1,2}";
-	
-}
-public static String mobileIsraelPhone() {
-	 return "\\+972-(5[0-9])-\\d{7}|05[0-9]-\\d{7}";
-}
+	public static String javaVariable() {
+		String regex = "[A-Za-z$][\\w$]*|_[\\w$]+";
+		return regex;
+	}
+
+	public static String zero_300() {
+		String regex = "[1-9]\\d?|[1-2]\\d\\d|300|0";
+		return regex;
+	}
+
+	public static String ipOctet() {
+		return "25[0-5]|2[0-4][0-9]|[01]?[0-9]{1,2}";
+
+	}
+
+	public static String mobileIsraelPhone() {
+		String regex = "(\\+972-5|05)[0-9]-\\d{7}";
+		return regex;
+
+	}
 }
